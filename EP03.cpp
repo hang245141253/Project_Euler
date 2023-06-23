9547f8a51589cf7f091ec7027f243e96d4a79dec
hang245141253 <hang245141253@gmail.com>
2023-06-23 15:43:13 +0800

    update

--- EP03.cpp
+++ EP03.cpp
@@ -5,15 +5,16 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143?
 */
 #include <cstdio>
+#define N 600851475143LL
 
 int main() {
     // 求m的最大质因数
-    long long m = 600851475143;
-    int k = 2; //即算法中描述的k
-    while (m > 1) {
-        if (!(m % k)) m /= k;
-        else ++k; // k是递增的
+    long long num = N;
+    int i = 2; //即算法中描述的k
+    while (num > 1) {
+        if (!(num % i)) num /= i;
+        else i++; // k是递增的
     }
-    printf("%d\n", k);
+    printf("%d\n", i);
     return 0;
 }
